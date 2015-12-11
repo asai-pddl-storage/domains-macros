65bb5d7e60f13d092183c60192abb588d277d865
Masataro Asai <guicho2.71828@gmail.com>
2015-12-11 15:46:42 +0900

    job.sh

--- job.sh
+++ job.sh
@@ -6,6 +6,7 @@ echodo (){
 
 echodo git checkout master
 echodo git branch split-$1
+echodo git checkout split-$1
 find . -name "*.plan" | while read p ; do ../../app/split-plan $p $1 ; done
 echodo git add --all
 echodo git commit -m "split-$1"
