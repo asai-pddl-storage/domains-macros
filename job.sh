03c0c31c4d1a1201ad59c345540ec525e01363ca
Masataro Asai <guicho2.71828@gmail.com>
2015-12-11 15:40:10 +0900

    job.sh

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 
 git checkout master
 git branch split-$1
-find . -name "*.plan" | while read p ; do ../../app/split-plan $p 3 ; done
+find . -name "*.plan" | while read p ; do ../../app/split-plan $p $1 ; done
 git add --all
 git commit -m "split-$1"
